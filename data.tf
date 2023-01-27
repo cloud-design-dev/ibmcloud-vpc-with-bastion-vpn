346831fe89d67e639b3fd41fdf0c417f14f36414
greyhoundforty <ryantiffany@fastmail.com>
2023-01-26 21:12:31 -0600

    Removing data jq source

--- data.tf
+++ data.tf
@@ -5,11 +5,6 @@ data "ibm_resource_group" "resource_group" {
   name = (var.resource_group != "" ? var.resource_group : "default")
 }
 
-# This is mainly for testing and debugging
-data "external" "env" {
-  program = ["jq", "-n", "env"]
-}
-
 # Pull in the zones in the region
 data "ibm_is_zones" "regional" {
   region = local.region
